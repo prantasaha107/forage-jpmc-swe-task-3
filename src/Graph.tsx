651e4a7eaac6e90af41d61f7a31997e82f3a92a9
Pranta Saha (prs527) <prs527@mail.usask.ca>
2023-09-10 16:03:38 -0600

    The schema is updated , This no loger counts top_asking_price instead finds out the ratio, The graph now implements the ratio and the trigger_alert.

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import {Table, TableData} from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -21,37 +21,46 @@ class Graph extends Component<IProps, {}> {
   componentDidMount() {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
-
+//Updated the schema with price, ratio , upper &lower bound and with trigger alert
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc:'float',
+      price_def:'float',
+      ratio:'float',
+      upper_bound: 'float',
+      lower_bound: 'float',
       timestamp: 'date',
+      trigger_alert:'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
+    //Updated more elements
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio","lower_bound","upper_bound","trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc:'avg',
+        price_def:'avg',
+        ratio:'avg',
         timestamp: 'distinct count',
+        lower_bound: 'avg',
+        upper_bound: 'avg',
+        trigger_alert:'avg',
       }));
     }
   }
 
+  /**
+   * when the graph gets new data. The
+   * change we want to make is on the argument we put in this.table.update.
+   */
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
+      this.table.update([ DataManipulator.generateRow(this.props.data),] as unknown as TableData
       );
     }
   }
